6d1150134e7cddd676956c55333c85b928086bd8
César D. Rodas <crodas@php.net>
2012-09-10 19:42:27 -0400

    Added missing test

---
+++ tests/input/class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *  @Route("/prefix")
+ */
+class SomeClass
+{
+    /** @Route("/some") */
+    public function index($Request)
+    {
+        $Request->set('controller', __METHOD__);
+        return __METHOD__;
+    }
+
+    /** @Method POST */
+    public function save()
+    {
+        return __METHOD__;
+    }
+}
