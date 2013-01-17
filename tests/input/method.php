a1df2fe2ec1773a164dafe05397cd60011585f45
César D. Rodas <crodas@php.net>
2013-01-17 05:54:06 -0300

    added missing file

---
+++ tests/input/method.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @Route("/foobar/12345") */
+class SomeMethodController
+{
+    /** @Method GET */
+    public function get($req)
+    {
+        $req->set('method', __METHOD__);
+        return __METHOD__;
+    }
+
+    /** @Method POST @Method DELETE */
+    public function modify($req)
+    {
+        $req->set('method', __METHOD__);
+        return __METHOD__;
+    }
+
+    /** @Route("/something") @Method POST @Method DELETE */
+    public function modify_something($req)
+    {
+        $req->set('method', __METHOD__);
+        return __METHOD__;
+    }
+}
