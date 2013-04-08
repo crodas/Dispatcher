a463fbdbdc4f0ce97d7753e6dfaa23ec5c1bc7d9
César D. Rodas <crodas@php.net>
2013-04-08 02:58:03 -0300

    use composer until Eath2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
-require __DIR__ . "/../packages/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . "/../lib/Dispatcher/autoload.php";
 
 foreach (glob(__DIR__ . "/generated/*") as $file) {
     unlink($file);
