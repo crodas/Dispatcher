06f2ca83ce141236b944a50ac36ba394e0b913fe
César D. Rodas <crodas@php.net>
2013-08-24 16:32:56 -0400

    fixed few service.php

--- service.php
+++ service.php
@@ -7,14 +7,13 @@ namespace Dispatcher\Service;
  *      dir: { type: 'hash', required: true},
  *      name: { default: 'generated'},
  *      ns:  { default: 'Dispatcher\Generated'},
- *      path: { require: true},
  *      temp_dir: { default: '/tmp' },
  *      devel: {default: true}
  *  }, {shared: true})
  */
 function dispatcher_service($config)
 {
-    $router = new \Dispatcher\Router( $confi['temp_dir'] . '/dispatcher:' . $config['name']  . '.php');
+    $router = new \Dispatcher\Router( $config['temp_dir'] . '/dispatcher:' . $config['name']  . '.php');
     foreach ($config['dir'] as $dir) {
         $router->addDirectory($dir);
     }
@@ -22,6 +21,8 @@ function dispatcher_service($config)
     if ($config['devel']) {
         $router->development();
     }
+
+    $router->load();
 
     return $router;
 }
